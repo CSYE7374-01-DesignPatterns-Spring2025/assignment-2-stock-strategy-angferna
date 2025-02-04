34fbb0992e3dfb973b68a07420cfc54337469528
angferna <angelnfernandes@gmail.com>
2025-02-03 22:08:25 -0500

    Updated Driver.java

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -2,7 +2,7 @@ package edu.neu.csye7374;
 
 /**
  * 
- * @author Yesha
+ * @author Angel Fernandes
  * 
  */
 
@@ -10,8 +10,8 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
 
-         //Add your code in between these two print statements
-		 
+        //Add your code in between these two print statements
+		StockMarket.demo();
 		System.out.println("\n\n============Main Execution End===================");
 	}
 
